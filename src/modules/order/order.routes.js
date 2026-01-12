dfa83265da6b64633deed1223ca8794473ad003b
arpitshukla267 <shuklaarpit440@gmail.com>
2026-01-12 17:02:37 +0530

    Add admin routes for order management

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router()
 const { createOrder, getOrders, getRemainingOrders, getOrderById, downloadInvoice, completeOrder, requestRefund, requestExchange } = require("./order.controller")
 const auth = require("../../middlewares/auth.middleware")
 
+// Admin routes (MUST be before other routes to avoid conflicts)
+router.use("/admin", require("./order.admin.routes"))
+
 // All order routes require authentication
 router.get("/remaining", auth, getRemainingOrders) // Must be before /:id route
 router.get("/", auth, getOrders)
